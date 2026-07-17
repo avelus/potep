b79342f918bdf24f96d38b632f963417a61959bd
avelus <ales.veluscek@gmail.com>
2026-07-17 14:40:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -377,35 +377,49 @@ function gpsSuccess(pos) {
         }
     }
 
-    if (!userMarker) {
+if (!userMarker) {
 
-        userMarker =
-            L.circleMarker(
-                [
-                    userPos.lat,
-                    userPos.lon
-                ],
-                {
-                    radius: 8,
-                    color: "blue",
-                    fillColor: "blue",
-                    fillOpacity: 0.9
-                }
-            ).addTo(map);
+    userMarker =
+        L.circleMarker(
+            [
+                userPos.lat,
+                userPos.lon
+            ],
+            {
+                radius: 8,
+                color: "blue",
+                fillColor: "blue",
+                fillOpacity: 0.9
+            }
+        ).addTo(map);
 
-    } else {
+    const startWp =
+        wps.find(
+            w => w.name === "START"
+        );
 
-        userMarker.setLatLng([
-            userPos.lat,
-            userPos.lon
-        ]);
+    if (startWp) {
+
+        const bounds =
+            L.latLngBounds([
+                [userPos.lat, userPos.lon],
+                [startWp.lat, startWp.lon]
+            ]);
+
+        map.fitBounds(
+            bounds,
+            {
+                padding: [50, 50]
+            }
+        );
     }
 
-    if (
-        gpsAccuracy <= 30
-    ) {
-        checkWaypoint();
-    }
+} else {
+
+    userMarker.setLatLng([
+        userPos.lat,
+        userPos.lon
+    ]);
 }
 
 function gpsError(err) {
